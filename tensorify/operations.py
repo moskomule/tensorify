0834ffd4037ccf2e8dffc2d4e8fea5316f32fdc2
moskomule <hataya@keio.jp>
2019-03-18 14:18:25 +0900

    add kronecker_prod and fix problems

--- tensorify/operations.py
+++ tensorify/operations.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import Sequence, Tuple, Optional
 
 import torch
 
@@ -14,9 +14,41 @@ def is_matrix(input: torch.Tensor):
     return input.dim() == 2
 
 
+def matricization(input: torch.Tensor, mode: int):
+    r""" Mode-n matricization of the input tensor as :math:`\mathbb{R}^{I_n\times I_1\dotsI_{n-1}I_{n+1}\dots I_N}`
+
+    :param input:
+    :param mode:
+    :return:
+    """
+    if input.dim() <= mode:
+        raise ValueError(f"`mode` is expected to be < {input.dim()} but got {mode}")
+    if input.dim() > 26:
+        raise RuntimeError("Dimension larger than 26 is not supported")
+    input_alp = "abcdefghijklmnopqrstuvwxyz"[:input.dim()]
+    mode_alp = input_alp[mode]
+    new_input_alp = mode_alp + input_alp.replace(mode_alp, "")
+    return torch.einsum(f"{input_alp}->{new_input_alp}",
+                        # to make sure the contiguity
+                        input).reshape(input.size(mode), -1)
+
+
+def canonical_matricization(input: torch.Tensor, mode: int):
+    r""" Mode-n canonical matricization of the input tensor as :math:`\mathbb{R}^{I_1\dotsI_n\timesI_{n+1}\dots I_N}`
+
+    :param input:
+    :param mode:
+    :return:
+    """
+    if input.dim() - 1 <= mode:
+        raise ValueError(f"`mode` is expected to be < {input.dim() - 1} but got {mode}")
+    input_size = torch.tensor(input.size())
+    return input.view(torch.prod(input_size[:mode + 1]), torch.prod(input_size[mode + 1:]))
+
+
 def product(input: torch.Tensor,
             other: torch.Tensor,
-            mode: int):
+            mode: int) -> torch.Tensor:
     r""" Mode-n product of a tensor :math:`\mathrm{input}\in\mathbb{R}^{I_1\times I_2\times\dots\timesI_N}` and
     a matrix :math:`\mathrm{other}\in\mathbb{J\times I_n}` or a vector :math:`\mathrm{other}\in\mathbb{I_n}`.
 
@@ -38,7 +70,7 @@ def product(input: torch.Tensor,
 
 
 def multilinier_product(core: torch.Tensor,
-                        factors: Sequence[torch.Tensor]):
+                        factors: Sequence[torch.Tensor]) -> torch.Tensor:
     r""" Multilinier product of a core tensor :math:`\mathrm{core}` and factor matrices
     :math:`\mathrm{factor}_0, \mathrm{factor}_1, \dots,`,
 
@@ -56,21 +88,26 @@ def multilinier_product(core: torch.Tensor,
 
 
 def contracted_product(input: torch.Tensor,
-                       other: torch.Tensor):
+                       other: torch.Tensor,
+                       dims: Optional[Tuple[int, int]] = None) -> torch.Tensor:
     """ Mode-(N, 1) contracted product of two tensors
 
     :param input:
     :param other:
+    :param dims:
     :return:
     """
-    if input.size(-1) != other.size(0):
-        raise ValueError(f"-1st mode of `input` and 0th mode of `other` should be same")
-    out_shape = input.size()[:-1] + other.size()[1:]
-    return input.view(-1, input.size(-1)).matmul(other.view(other.size(0), -1)).view(out_shape)
+    if dims is None:
+        dims = [input.dim() - 1, 0]
+    if len(dims) != 2:
+        raise ValueError(f"`dims` should be a tuple of a pair of integers")
+    if input.size(dims[0]) != other.size(dims[1]):
+        raise ValueError("-1st mode of `input` and 0th mode of `other` should be same")
+    return torch.tensordot(input, other, [[dims[0]], [dims[1]]])
 
 
 def outer_product(input: torch.Tensor,
-                  other: torch.Tensor):
+                  other: torch.Tensor) -> torch.Tensor:
     """ Outer product of two tensors
 
     :param input:
@@ -78,7 +115,7 @@ def outer_product(input: torch.Tensor,
     :return:
     """
     if input.dim() > 13 or other.dim() > 13:
-        raise RuntimeError("Dimension larger than 26 is not supported")
+        raise RuntimeError("Dimension larger than 13 is not supported")
     input_alp = "abcdefghijklm"[:input.dim()]
     other_alp = "nopqrstuvwxyz"[:other.dim()]
     return torch.einsum(f"{input_alp},{other_alp}->{input_alp}{other_alp}",
@@ -87,11 +124,20 @@ def outer_product(input: torch.Tensor,
 
 
 def kronecker_product(input: torch.Tensor,
-                      target: torch.Tensor):
+                      other: torch.Tensor) -> torch.Tensor:
     """ Left Kronecker product of two tensors
 
     :param input:
-    :param target:
+    :param other:
     :return:
     """
-    raise NotImplementedError
+    if input.dim() != other.dim():
+        raise ValueError("The dimensions of `input` and `other` should be same")
+    input_size = torch.tensor(input.size())
+    other_size = torch.tensor(other.size())
+
+    # input_size (a, b, c) -> (a, 1, b, 1, c, 1)
+    new_input_size = torch.stack([input_size, torch.ones_like(input_size)], dim=1).view(-1).tolist()
+    new_other_size = torch.stack([torch.ones_like(other_size), other_size], dim=1).view(-1).tolist()
+    expected_size = (input_size * other_size).tolist()
+    return (input.view(new_input_size) * other.view(new_other_size)).view(expected_size)
--- test/test_operations.py
+++ test/test_operations.py
@@ -4,10 +4,29 @@ import torch
 from tensorify import operations
 
 
+def test_matricization():
+    input = torch.randn(4, 3, 2, 5, requires_grad=True)
+    matrix = operations.matricization(input, 2)
+    assert matrix.size() == torch.Size([2, 60])
+    # check if backward-able
+    matrix.sum().backward()
+
+
+def test_canonical_matricization():
+    input = torch.randn(4, 3, 2, 5, requires_grad=True)
+    matrix = operations.canonical_matricization(input, 2)
+    assert matrix.size() == torch.Size([24, 5])
+    # check if backward-able
+    matrix.sum().backward()
+
+
 def test_product():
-    input = torch.randn(4, 3, 2)
+    input = torch.randn(4, 3, 2, requires_grad=True)
     other = torch.randn(4, 3)
-    assert operations.product(input, other, 1).size() == torch.Size([4, 4, 2])
+    product = operations.product(input, other, 1)
+    assert product.size() == torch.Size([4, 4, 2])
+    # check if backward-able
+    product.sum().backward()
 
     other = torch.randn(3)
     assert operations.product(input, other, 1).size() == torch.Size([4, 2])
@@ -21,25 +40,49 @@ def test_product():
 
 
 def test_multilinier_product():
-    core = torch.randn(4, 3, 2)
-    factors = [torch.randn(3, 4),
-               torch.randn(3, 3),
-               torch.randn(3, 2)]
-    assert operations.multilinier_product(core, factors).size() == torch.Size([3, 3, 3])
+    core = torch.randn(4, 3, 2, requires_grad=True)
+    factors = [torch.randn(3, 4, requires_grad=True),
+               torch.randn(3, 3, requires_grad=True),
+               torch.randn(3, 2, requires_grad=True)]
+    product = operations.multilinier_product(core, factors)
+    assert product.size() == torch.Size([3, 3, 3])
+    # check if backward-able
+    product.sum().backward()
+
     factors.pop()
     with pytest.raises(ValueError):
         operations.multilinier_product(core, factors)
 
 
 def test_contracted_product():
-    input = torch.randn(4, 3, 2)
+    input = torch.randn(4, 3, 2, requires_grad=True)
     other = torch.randn(2, 3, 4)
-    assert operations.contracted_product(input, other).size() == torch.Size([4, 3, 3, 4])
+    product = operations.contracted_product(input, other, None)
+    assert product.size() == torch.Size([4, 3, 3, 4])
+    # check if backward-able
+    product.sum().backward()
     with pytest.raises(ValueError):
-        operations.contracted_product(input, torch.randn(3, 4, 2))
+        operations.contracted_product(input, torch.randn(3, 4, 2), dims=(2, 0))
 
 
 def test_outer_prodcut():
-    input = torch.randn(4, 3, 2)
+    input = torch.randn(4, 3, 2, requires_grad=True)
     other = torch.randn(2, 3, 4)
-    assert operations.outer_product(input, other).size() == torch.Size([4, 3, 2, 2, 3, 4])
+    product = operations.outer_product(input, other)
+    assert product.size() == torch.Size([4, 3, 2, 2, 3, 4])
+    # check if backward-able
+    product.sum().backward()
+
+
+def test_kronecker_product():
+    input = torch.randn(4, 3, 2)
+    other = torch.randn(3, 3, 3)
+    input[0, 0, 0] = 1
+    input.requires_grad_()
+    product = operations.kronecker_product(input, other)
+    # size check
+    assert product.size() == torch.Size([4 * 3, 3 * 3, 2 * 3])
+    # value check
+    assert torch.equal(product[0:3, 0:3, 0:3], other)
+    # check if backward-able
+    product.sum().backward()
